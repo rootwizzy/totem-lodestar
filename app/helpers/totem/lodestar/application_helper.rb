6fe6e1078b4b4fbc127ddff86407f6cbedd98c3e
Zachary Johnson <rootwizzy@gmail.com>
2016-10-31 17:56:47 -0500

    [ADD] New stylings for totem-docs

--- app/helpers/totem/lodestar/application_helper.rb
+++ app/helpers/totem/lodestar/application_helper.rb
@@ -5,7 +5,7 @@ module Totem
 
       class CodeRayify < Redcarpet::Render::HTML
         def block_code(code, language)
-          CodeRay.scan(code, language).div
+          CodeRay.scan(code, language).div(css: :class)
         end
       end
 
