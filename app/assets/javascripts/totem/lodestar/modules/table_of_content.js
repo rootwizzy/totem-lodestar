94d737b62e204b06161f24a9c766def9a4b12536
Zachary Johnson <rootwizzy@gmail.com>
2016-11-02 11:42:14 -0500

    [ADD] Transition Property for ToC Style

--- app/assets/javascripts/totem/lodestar/modules/table_of_content.js
+++ app/assets/javascripts/totem/lodestar/modules/table_of_content.js
@@ -3,50 +3,54 @@ TotemLodestar._modules['table_of_contents'] = {
     TotemLodestar.load_module(this, options)
   },
 
+
   smooth_scroll: function(options) {
     speed = options['speed']
 
-    $('a[href^="#"]').on('click',function (e) {
+    $('a[href^="#"]').on('click', function (e) {
       e.preventDefault();
 
       var target = this.hash;
       var $target = $(target);
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, speed, 'swing', function () {
-          window.location.hash = target;
-      });
+      $('html, body').stop().animate(
+        {'scrollTop': $target.offset().top}, 
+        speed, 
+        'swing', 
+        function () {window.location.hash = target;}
+      );
     });
   },
 
   highlight: function(options) {
-    ticking       = false;
-    region_lookup = []
-    cur_region    = null
+    ticking    = false;
+    regions    = []
+    cur_region = null
 
-    set_document_positions();
+    build_document_positions();
+    check_header_pos(window.scrollY) // Initialize to set the first region active
 
     window.addEventListener('scroll', function(e){
-      last_known_scroll_position = window.scrollY;
       if (!ticking) {
         window.requestAnimationFrame(function() {
-          check_header_pos(last_known_scroll_position)
+          check_header_pos(window.scrollY)
           ticking = false;
         });
       }
       ticking = true;
     }) 
 
-    function set_document_positions() {
-      headers = $("h1, h2, h3, h4, h5, h6")
-
-      headers.each(function(i){
-        region_lookup.push({top: 0, bottom: $(this)[0].offsetTop, $: $(this)});
-      }, this)
+    function build_document_positions() {
+      $("h1, h2, h3, h4, h5, h6").each(function() { 
+        regions.push({ 
+          top:    0, 
+          bottom: $(this)[0].offsetTop, 
+          $:      $(this),
+        })
+      })
 
       prev = null
-      region_lookup.forEach(function(header){
+      regions.forEach(function(header){
         if(prev) { header.top = prev.bottom - 1 }
         prev = header
       })
@@ -54,26 +58,25 @@ TotemLodestar._modules['table_of_contents'] = {
 
     function check_header_pos(pos) {
       if(cur_region == null || pos < cur_region.top || pos > cur_region.bottom) { 
-        set_cur_region_from_pos(pos);
-        add_cur_region_to_toc();
+        set_cur_region(pos);
       }
     }
 
-    function set_cur_region_from_pos(pos) {
-      region_lookup.forEach(function(header){
+    function set_cur_region(pos) {
+      regions.forEach(function(header){
         if(pos >= header.top && pos <= header.bottom) {
           cur_region = header
         }
-      })
+      });
+     activate_toc_header();
+
     }
 
-    function add_cur_region_to_toc() {
+    function activate_toc_header() {
       header_href = "#" + cur_region.$[0].id
       $('.toc_list').children('a[href="' + header_href +'"]').addClass('active');
       $('.toc_list').children(':not(a[href="' + header_href +'"])').removeClass('active');
     }
-
-
   },
 }
 
